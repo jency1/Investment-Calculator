24934382a42bb4ac41a0b415ed65fd2daa9ddc6f
jency <jencymaheshwari2004@gmail.com>
2024-09-05 00:47:14 +0530

    Investment Calculator

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+
+function App() {
+  const [userInput, setUserinput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserinput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue, // + is used to change the string value to a number
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
+}
+
+export default App;
